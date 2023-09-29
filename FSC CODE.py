923aa20bf412e3e75ebfe20589a5e15e8d71a45f
Carlosw5 <142521048+Carlosw5@users.noreply.github.com>
2023-09-29 11:06:01 -0400

    Update FSC CODE.py

--- FSC CODE.py
+++ FSC CODE.py
@@ -2,164 +2,168 @@ import time
 import board
 import neopixel
 import random
-np = neopixel.NeoPixel(board.D2, 30, auto_write = False, brightness = 0.5)
-red = [255, 0, 0]
-blue = [0, 43, 255]
-pink = [255, 0, 255]
-green = [0, 153, 0]
-turq = [0, 255, 255]
-purple = [255, 0, 255]
-orange = [255, 128, 0]
-color = [255,156,127]
 
 times = 1
+
+np = neopixel.NeoPixel(board.NEOPIXEL, 10, auto_write = False, brightness = 1)
+
+red = (255, 0, 0)
+black = (0,0,0)
+white = (255,255,255)
+blue = (0,0,255)
+green = (0,255,0)
+purple = (255,0,255)
+yellow = (255, 100, 0)
+orangeT = (255, 80, 0)
+orange = (255, 64, 0)
+lightBlue = (87, 232, 255)
+lightpurple = (227, 98, 255)
+darkpurple = (18,0,18)
+defaultcolor = [216, 231, 0]
+defcolor = [orange, yellow, orangeT]
+randcolor = random.choice(defcolor)
+defcolor2 = [lightBlue, lightBlue, white]
+randcolor2 = random.choice(defcolor2)
+
+color1 = [defaultcolor[0],defaultcolor[1],defaultcolor[2]]
+np.fill(color1)
 '''
+Function: fade_out
 
-Function: Fade Out
+Description: This function begins with a color and fades to black
 
-Description: This function decreases the brightness of the neopixel from its original brightness by lowering RGB value
+Parameters: defcolor(list), delay(float)
 
-Parameters: color and speed
-
-Return value: None
-
+Return value: Prints the color values as they update
 '''
-def fadeOut(color, speed=0.001):
-    if speed <= 0:
-        speed = 1
-    fadeR = color[0]/256.0
-    fadeG = color[1]/256.0
-    fadeB = color[2]/256.0
-    color1 = [color[0],color[1],color[2]]
-    np.fill(color1)
-    np.show()
-    for i in range(255):
-        color1[0] = int (color[0] - (fadeR*i))
-        color1[1] = int (color[1] - (fadeG*i))
-        color1[2] = int (color[2] - (fadeB*i))
+def fade_out(defaultcolor, delay = 0.005):
+    fadeR = defaultcolor[0]/256.0
+    fadeG = defaultcolor[1]/256.0
+    fadeB = defaultcolor[2]/256.0
+    for i in range(256):
+        color1[0] = int (defaultcolor[0] - (fadeR*i))
+        color1[1] = int (defaultcolor[1] - (fadeG*i))
+        color1[2] = int (defaultcolor[2] - (fadeB*i))
         np.fill(color1)
+        print(i, defaultcolor,fadeR*i,fadeG*i,fadeB*i)
+        time.sleep(delay)
         np.show()
-        print(color1)
-        time.sleep(speed)
-'''
-
-Function: Fade In
-
-Description: This function increases brighteness of a neopixel by raising RGB value
-
-Parameters: color, speed
-
-Return value: none
 
 '''
-def fadeIn(color, speed=0.001):
-    if speed <= 0:
-        speed = 1
-    fadeR = color[0]/256.0
-    fadeG = color[1]/256.0
-    fadeB = color[2]/256.0
-    color1 = [0,0,0]
-    np.fill(color1)
-    np.show()
-    print(color1)
-    for i in range(255):
+Function: fade_in
+
+Description: This function begins with a black and fades in to a color
+
+Parameters: defcolor(list), delay(float)
+
+Return value: Prints the color values as they update
+'''
+def fade_in(defaultcolor, delay = 0.005):
+    fadeR = defaultcolor[0]/256.0
+    fadeG = defaultcolor[1]/256.0
+    fadeB = defaultcolor[2]/256.0
+    for i in range(256):
         color1[0] = int (fadeR*i)
         color1[1] = int (fadeG*i)
         color1[2] = int (fadeB*i)
         np.fill(color1)
+        print(i, defaultcolor,fadeR*i,fadeG*i,fadeB*i)
+        time.sleep(delay)
         np.show()
-        time.sleep(speed)
-        print(color1)
-
+        
 '''
+Function: sparkle
 
-Function: Chase
+Description: This function causes a foreground color to pop up in random pixel intervals on top of the background color.
 
-Description: Takes in a background color and a set color moves in intervals of 2
+Parameters: fgcolor(list), bgcolor(list), delay(float), num_sparks(int)
 
-Parameters: color and speed
-
-Return value: background color
-
+Return value: nothing
 '''
-def chase(color = [0,0,0], speed = 0.01):
-    for j in range(30):
+def sparkle(fgcolor = white, bgcolor = black, delay = 0.005, num_sparks = 10):
+    for i in range(50):
+        np.fill(bgcolor)
         np.show()
-        for i in range(30):
+        for j in range(num_sparks):
+            rand_int = random.randrange(0, 10)
+            np[rand_int] = fgcolor
+            np.show()
+        time.sleep(delay)
+        np[rand_int] = bgcolor
+        np.show()
+        
+'''
+Function: chase
+
+Description: This function causes a foreground color to follow after the background in sets of two
+
+Parameters: fgcolor(list), bgcolor(list), speed(float)
+
+Return value: Prints the background and foreground color values as they update
+'''
+def chase(fgcolor = green, bgcolor = purple, speed = 0.005):
+    for j in range(100):
+        np.show()
+        for i in range(10):
             if i % 3 != 0:
-                led = (i+j) % 30 
-                np[led] = [255,0,255]
+                led = (i+j) % 10 
+                np[led] = fgcolor
                 print("bColor",i,np[i])
             elif i % 3 == 0:
-                led = (i+j) % 30
-                np[led] = color
+                led = (i+j) % 10
+                np[led] = bgcolor
                 print("fColor",i,np[i])
             time.sleep(speed)
-'''
 
-Function: Sparkle
-
-Description: This function takes in a background color and then uses a random integer to pop a random color
-
-Parameters: color and times
-
-Return value: None
-
-'''
-def sparkle(color = [0,0,0], times = 1):
-    for i in range(times):
-        np.fill(color)
-        led1 = random.randint(0, 28)
-        led2 = random.randint(0, 28)
-        led3 = random.randint(0, 28)
-        np[led1] = [12,124,42]
-        np[led2] = [255,255,255]
-        np[led3] = [255,255,255]
+def lightning(bgcolor = darkpurple, flash = randcolor2, numpix = np.n):
+    for i in range(50):
+        randomsleep = random.random()*10
+        randompix = random.randrange(3,8)
+        print("sleep", randomsleep)
+        np.fill(bgcolor)
         np.show()
-        time.sleep(0.7)
-
-while True:
-    sparkle(pink, 5)
-    fadeIn(blue)
-    fadeOut(blue)
-    fadeIn(red)
-    fadeOut(red)
-    chase(turq)
-    fadeIn(purple)
-    fadeOut(purple)
-    fadeIn(orange)
-    fadeOut(orange)
-    chase(orange)
-    sparkle(red, 5)
-
-import time
-import board
-import neopixel
-import random
-np = neopixel.NeoPixel(board.D2, 30, auto_write = False, brightness = 0.5)
-red = [255, 0, 0]
-blue = [0, 43, 255]
-pink = [255, 0, 255]
-green = [0, 153, 0]
-turq = [0, 255, 255]
-purple = [255, 0, 255]
-orange = [255, 128, 0]
-color = [255,156,127]
-black = [0, 0, 0]
-
-times = 1
-
-def sparkle(color = [0,0,0], times = 1):
-    for i in range(times):
-        np.fill(color)
-        for i in range(3):
-            np[random.randint(0, 28)] = [102,0,204]
-            np[random.randint(0, 28)] = [255, 255, 255]
+        time.sleep(randomsleep)
+        for j in range(randompix):
+            np.fill(flash)
             np.show()
-            time.sleep(random.random())
-    
-        
-while True:
-    sparkle(black, 8)
+            time.sleep(0.05)
+            np.fill(bgcolor)
+            np.show()
+            time.sleep(0.01)
 
-    
+def fire(fgcolor = white, bgcolor = black, num_sparks = 15):
+    for i in range(50):
+        np.fill(bgcolor)
+        np.show()
+        for j in range(num_sparks):
+            randcolor = random.choice(defcolor)
+            rand_int = random.randrange(0, 10)
+            rand_intT = random.randrange(0, 10)
+            randomsleep = random.random()/100
+            np[rand_int] = fgcolor
+            np[rand_intT] = black
+            np.show()
+            time.sleep(randomsleep)
+        np[rand_int] = bgcolor
+        np.show()
+
+while True:
+    fade_out(purple)
+    fade_in(green)
+    fade_out(green)
+    fade_in(purple)
+    fade_out(purple)
+    fade_in(green)
+    sparkle(purple, green, 0.05, 5)
+    fade_out(green)
+    fade_in(purple)
+    fade_out(purple)
+    fade_in(green)
+    chase()
+    fade_out(green)
+    fade_in(orange)
+    chase(orange, purple)
+    fade_out(purple)
+    fade_in(purple)
+    fire(randcolor, red)
+    #lightning()
